e8874fa1863b13e4ded920447b1eafcb933102a6
Ruslan <ruslan.grudistov@gmail.com>
2024-02-25 13:35:07 +0200

    1

--- src/components/Contact/Constact.jsx
+++ src/components/Contact/Constact.jsx
@@ -1,20 +1,24 @@
 import css from "./Contact.module.css";
 
 export const ContactRender = ({ onClick, contacts }) => {
-  return contacts.map((el) => (
-    <li key={el.id}>
-      {el.name}: {el.number}
-      <button
-        name={el.id}
-        onClick={(e) => {
-          onClick(e.target.name);
-        }}
-        className={css.delete}
-      >
-        Delete
-      </button>
-    </li>
-  ));
+  return (
+    <ul>
+      {contacts.map((el) => (
+        <li key={el.id}>
+          {el.name}: {el.number}
+          <button
+            name={el.id}
+            onClick={(e) => {
+              onClick(e.target.name);
+            }}
+            className={css.delete}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ContactRender;
